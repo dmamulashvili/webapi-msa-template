261e1760bc54088506afd9dd8c8ac762aa7f4547
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-07-11 23:03:22 +0400

    Comment JWT Audience

--- src/MSA.Template.API/Program.cs
+++ src/MSA.Template.API/Program.cs
@@ -33,8 +33,8 @@ builder.Services.AddAuthentication(options =>
         cfg.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateLifetime = true,
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            // ValidateAudience = true,
+            // ValidAudience = builder.Configuration["JWT:ValidAudience"],
             ValidateIssuer = true,
             ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
             ValidateIssuerSigningKey = true,
