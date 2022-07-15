75e370d560480749f0cc45042edfd6804981eebf
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-07-15 23:58:50 +0400

    Add JsonIgnore to CorrelationId

--- src/MSA.Template.SharedKernel/BaseCommand.cs
+++ src/MSA.Template.SharedKernel/BaseCommand.cs
@@ -1,9 +1,11 @@
 using System;
 using MediatR;
+using System.Text.Json.Serialization;
 
 namespace MSA.Template.SharedKernel;
 
 public abstract class BaseCommand<TResponse> : IRequest<TResponse>
 {
+    [JsonIgnore]
     public Guid CorrelationId { get; set; }
 }
