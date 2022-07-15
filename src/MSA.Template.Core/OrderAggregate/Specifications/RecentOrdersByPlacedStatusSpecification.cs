038545a9ee4be7a2d6f4065efbf9072822f06f9f
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-07-15 23:57:09 +0400

    Change Spec

---
+++ src/MSA.Template.Core/OrderAggregate/Specifications/RecentOrdersByPlacedStatusSpecification.cs
@@ -0,0 +1,12 @@
+using MSA.Template.SharedKernel;
+
+namespace MSA.Template.Core.OrderAggregate.Specifications;
+
+public sealed class RecentOrdersByPlacedStatusSpecification : BaseSpecification<Order>
+{
+    public RecentOrdersByPlacedStatusSpecification() : base(o => o.OrderStatus == OrderStatus.Placed)
+    {
+        AddInclude(o => o.OrderLines);
+        ApplyOrderByDescending(o => o.OrderDate);
+    }
+}
