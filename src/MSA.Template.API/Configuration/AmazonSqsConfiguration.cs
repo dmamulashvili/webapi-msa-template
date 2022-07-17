573cbc5e6f79adf970c38f53ae63c688e361bae2
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-07-17 17:51:38 +0400

    REF

---
+++ src/MSA.Template.API/Configuration/AmazonSqsConfiguration.cs
@@ -0,0 +1,9 @@
+namespace MSA.Template.API.Configuration;
+
+public class AmazonSqsConfiguration
+{
+    public string? AccessKey { get; set; }
+    public string? SecretKey { get; set; }
+    public string? RegionEndpointSystemName { get; set; }
+    public string? QueueName { get; set; }
+}
