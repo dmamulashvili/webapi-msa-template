2c24d68e9936f1ce3c69a167653939a06af6a2a4
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-07-17 17:39:38 +0400

    Remove MSA.Template.Infrastructure.AmazonSQS

--- src/MSA.Template.Infrastructure.AmazonSQS/Configuration/AmazonSqsConfiguration.cs
+++
@@ -1,9 +0,0 @@
-namespace MSA.Template.Infrastructure.AmazonSQS.Configuration;
-
-public class AmazonSqsConfiguration
-{
-    public string? AccessKey { get; set; }
-    public string? SecretKey { get; set; }
-    public string? RegionEndpointSystemName { get; set; }
-    public string? QueueName { get; set; }
-}
