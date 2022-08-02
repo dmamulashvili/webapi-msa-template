5f15ec07585f44d8596bbe18c6a5468dc1c488bf
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-08-02 23:17:40 +0400

    Comment _eventBus

--- src/MSA.Template.API/Services/IntegrationEventService.cs
+++ src/MSA.Template.API/Services/IntegrationEventService.cs
@@ -8,16 +8,18 @@ namespace MSA.Template.API.Services;
 public class IntegrationEventService : IIntegrationEventService
 {
     private readonly IServiceProvider _serviceProvider;
-    private readonly IBus _eventBus;
+    // private readonly IBus _eventBus;
     private readonly IIdentityService _identityService;
     private readonly ILogger<IntegrationEventService> _logger;
     private readonly List<IIntegrationEvent> _events = new List<IIntegrationEvent>();
 
-    public IntegrationEventService(IServiceProvider serviceProvider, IBus eventBus, IIdentityService identityService,
+    public IntegrationEventService(IServiceProvider serviceProvider, 
+        // IBus eventBus, 
+        IIdentityService identityService,
         ILogger<IntegrationEventService> logger)
     {
         _serviceProvider = serviceProvider;
-        _eventBus = eventBus;
+        // _eventBus = eventBus;
         _identityService = identityService;
         _logger = logger;
     }
