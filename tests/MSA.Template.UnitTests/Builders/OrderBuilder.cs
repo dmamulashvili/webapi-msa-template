770fb14166d0df624796ad263eb914f95ecd55e8
Dimitri Mamulashvili <dimitri.mamulashvili@pm.me>
2022-09-03 22:08:20 +0400

    Add OrderBuilder.cs

---
+++ tests/MSA.Template.UnitTests/Builders/OrderBuilder.cs
@@ -0,0 +1,23 @@
+﻿using MSA.Template.Core.OrderAggregate;
+
+namespace MSA.Template.UnitTests.Builders;
+
+public class OrderBuilder
+{
+    public Order CreateDraftOrder()
+    {
+        var address = CreateEmptyAddress();
+        return new Order(OrderStatus.Draft, DateTimeOffset.UtcNow, address);
+    }
+
+    public Order CreatePlacedOrder()
+    {
+        var address = CreateEmptyAddress();
+        return new Order(OrderStatus.Placed, DateTimeOffset.UtcNow, address);
+    }
+
+    public Address CreateEmptyAddress()
+    {
+        return new Address(string.Empty, string.Empty);
+    }
+}
