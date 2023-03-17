a70753952c56250cf71fce70604fefbfaa5c8aa6
Dimitri Mamulashvili <dimitri.mamulashvili@pm.me>
2023-03-18 01:05:06 +0400

    Remove AddEndpointsApiExplorer

--- src/MSA.Template.API/Program.cs
+++ src/MSA.Template.API/Program.cs
@@ -63,8 +63,6 @@ builder.Services.AddAuthentication(options =>
         };
     });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
